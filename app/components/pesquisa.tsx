b49ed8666bec8e32c2a738f3a1a8d76bc95b29c6
gabrielmalves7-prog <gabriel.malves7@aluno.edu.es.gov.br>
2026-06-23 21:40:39 +0000

    opa

--- app/components/pesquisa.tsx
+++ app/components/pesquisa.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 
-function Pesquisa({ children }: { children: React.ReactNode }) {
+function Pesquisa({ children }: { children: React.ReactNode },) {
   return (
-    <header className="w-full bg-white border-b border-gray-200 shadow-sm">
+    <header className="w-full bg-black border-b border-gray-200 shadow-sm">
       {/* max-w-7xl e mx-auto mantêm o conteúdo centralizado em telas muito grandes */}
-      <nav className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
+      <nav className="max-w-7xl mx-auto flex-1 items-center justify-between px-6 py-4">
         {children}
       </nav>
     </header>
--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <Pesquisa>
+      <Pesquisa className="font-geisSans">
         <Link href="/form">Formulário</Link>
         <Link href="/test">Test</Link>
       </Pesquisa>
