3f583bd2bb000e5610c6301f7121e63c0423fd81
gabrielmalves7-prog <gabriel.malves7@aluno.edu.es.gov.br>
2026-09-16 21:49:35 +0000

    arrumando css

--- app/fonte/page.tsx
+++ app/fonte/page.tsx
@@ -5,16 +5,19 @@ import estilo from './estilo.module.css';
 export default function page() {
   return (
     <html>
-        <body className={estilo.texto}>
+        <body> <div  className={estilo.texto}>
+
           <h3>Canal de Jogos Jogaveis</h3>
 
           <p> <span className={estilo.destaque}>SE INSCREVAM NO CANAL!!</span> </p>
+
           <p>
             Eai galerinha do Youtube!
           </p>
 
           <blockquote className={estilo.torto}>Esse canal é muito legal</blockquote>
-        </body>
+          
+        </div> </body>
     </html>
   )
 };
