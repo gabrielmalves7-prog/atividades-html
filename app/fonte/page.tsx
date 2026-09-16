60a4ed5deebd05c18b941a599ceadf5a9fae31f8
gabrielmalves7-prog <gabriel.malves7@aluno.edu.es.gov.br>
2026-09-16 21:14:39 +0000

    atividade 10.1

---
+++ app/fonte/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import estilo from './estilo.module.css';
+
+export default function page() {
+  return (
+    <html>
+        <body>
+          <p>
+            Eai galerinha do Youtube!
+          </p>
+        </body>
+    </html>
+  )
+};
